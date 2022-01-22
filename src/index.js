f2466809a93da4bcea9f178c947854517c881afe
Vitaliy Hrytsyuk <fahivchik1@gmail.com>
2022-01-22 21:58:13 +0200

    init: did the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0){
+        n = ''+(n * -1)
+    }else {
+        n = ''+ n
+    }
+    new_number = ''
+    len_n = n.length
+    for (let i = len_n; i >= 0; i--){
+      new_number += n.charAt(i);
+    }
+    return Number(new_number)
 }
